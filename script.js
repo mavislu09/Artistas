62821fa871723d7ebc7bf6f8e45faa54f4cbba67
mavislu09 <dannacastillo09@gmail.com>
2020-09-26 20:47:08 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,72 @@
+function kurt() {
+    let name= prompt("¿Quién es este artista?")
+    let nombre = document.getElementById("nombre")
+    nombre.textContent = name
+
+    let genero= prompt("¿A qué género musical pertenece?")
+    let musica = document.getElementById("musica")
+    musica.textContent = genero
+
+    let country= prompt("¿De qué país era?")
+    let pais = document.getElementById("pais")
+    pais.textContent = country
+
+    let cancion= prompt("Ingresa tu canción favorita")
+    let song = document.getElementById("song")
+    song.textContent = cancion
+}
+/* Top function */
+function twentyOnePilots() {
+    let nameT= prompt("¿Cuál es el nombre de esta banda?")
+    let nombreT = document.getElementById("nombreT")
+    nombreT.textContent = nameT
+
+    let generoT= prompt("¿A qué género musical pertenecen?")
+    let musicaT = document.getElementById("musicaT")
+    musicaT.textContent = generoT
+
+    let countryT= prompt("¿De qué país son?")
+    let paisT = document.getElementById("paisT")
+    paisT.textContent = countryT
+
+    let cancionT= prompt("Ingresa tu canción favorita")
+    let songT = document.getElementById("songT")
+    songT.textContent = cancionT
+}
+/* Mariana function */
+function froes() {
+    let nameM= prompt("¿Quién es esta artista?")
+    let nombreM = document.getElementById("nombreM")
+    nombreM.textContent = nameM
+
+    let generoM= prompt("¿A qué género musical pertenece?")
+    let musicaM = document.getElementById("musicaM")
+    musicaM.textContent = generoM
+
+    let countryM= prompt("¿De qué país es?")
+    let paisM = document.getElementById("paisM")
+    paisM.textContent = countryM
+
+    let cancionM= prompt("Ingresa tu canción favorita")
+    let songM = document.getElementById("songM")
+    songM.textContent = cancionM
+}
+
+/* Gorillaz function */
+function gorillaz() {
+    let nameG= prompt("¿Cuál es el nombre de esta banda?")
+    let nombreG = document.getElementById("nombreG")
+    nombreG.textContent = nameG
+
+    let generoG= prompt("¿A qué género musical pertenecen?")
+    let musicaG = document.getElementById("musicaG")
+    musicaG.textContent = generoG
+
+    let countryG= prompt("¿De qué país son?")
+    let paisG = document.getElementById("paisG")
+    paisG.textContent = countryG
+
+    let cancionG= prompt("Ingresa tu canción favorita")
+    let songG = document.getElementById("songG")
+    songG.textContent = cancionG
+}
